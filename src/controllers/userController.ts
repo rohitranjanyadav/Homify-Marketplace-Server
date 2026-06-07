ed77e889b276b15ee54ca036b8642eb24205f974
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-06-07 18:52:38 +0545

    feat: implement basic user login functionality with validation and password comparison

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -18,12 +18,49 @@ class UserController {
     await User.create({
       username,
       email,
-      password: bcrypt.hashSync(password,10),
+      password: bcrypt.hashSync(password, 10),
     });
 
     res.status(201).json({
       message: "User registered successfully",
     });
+  }
+
+  static async login(req: Request, res: Response) {
+    // Accept incoming data
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400).json({
+        message: "Please provide email and password",
+      });
+      return;
+    }
+
+    // Check if email exists
+    const [user] = await User.findAll({
+      where: {
+        email: email,
+      },
+    });
+
+    // If email exists only, verify password
+    if (!user) {
+      res.status(404).json({
+        message: "No user found with that email",
+      });
+    } else {
+      const isEqual = bcrypt.compareSync(password, user.password);
+      if (!isEqual) {
+        res.status(400).json({
+          message: "Invalid credentials",
+        });
+      } else {
+        res.status(200).json({
+          message: "Logged in",
+        });
+      }
+    }
   }
 }
 
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -4,5 +4,6 @@ import UserController from "../controllers/userController.ts";
 const router = express.Router();
 
 router.route("/register").post(UserController.register);
+router.route("/login").post(UserController.login);
 
 export default router;
