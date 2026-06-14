5949d2a488e14dc1e7fd01a44eb86f47ac1200ae
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-06-14 16:36:15 +0545

    feat: add type annotation for Router in userRoute

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,7 +1,7 @@
-import express from "express";
+import express, { Router } from "express";
 import UserController from "../controllers/userController.ts";
 
-const router = express.Router();
+const router:Router = express.Router();
 
 router.route("/register").post(UserController.register);
 router.route("/login").post(UserController.login);
