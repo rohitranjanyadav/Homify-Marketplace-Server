cb117bd1e0b990504f53a7462e287e853ec1df24
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-06-14 16:42:04 +0545

    feat: add category route for category management

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import "reflect-metadata";
 import express from "express";
 import "./database/connection.ts";
 import userRoute from "./routes/userRoute.ts";
+import categoryRoute from "./routes/categoryRoute.ts";
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/auth", userRoute);
+app.use("/api/category", categoryRoute);
 
 export default app;
---
+++ src/routes/categoryRoute.ts
@@ -0,0 +1,16 @@
+import express, { Router } from "express";
+import categoryController from "../controllers/categoryController.ts";
+
+const router: Router = express.Router();
+
+router
+  .route("/")
+  .get(categoryController.getCategories)
+  .post(categoryController.addCategory);
+
+router
+  .route("/:id")
+  .patch(categoryController.updateCategory)
+  .delete(categoryController.deleteCategory);
+
+export default router;
